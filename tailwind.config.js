c049aef8e7ae89af593d950c45b6e96c6b889bcd
Joan <joan300501@gmail.com>
2024-01-08 10:36:44 -0500

    Update tailwind.config.js with eslint-disable-next-line directive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
   darkMode: ["class"],
   content: [
@@ -35,5 +36,6 @@ module.exports = {
       },
     },
   },
+  // eslint-disable-next-line no-undef
   plugins: [require("tailwindcss-animate")],
 };
